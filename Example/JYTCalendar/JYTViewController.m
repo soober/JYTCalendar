966681d8a9867fdbafb968db617da472e19ef3e6
jiayuntian <jiayuntian@able-elec.com>
2021-07-02 16:40:13 +0800

    示例代码

--- Example/JYTCalendar/JYTViewController.m
+++ Example/JYTCalendar/JYTViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JYTViewController.h"
+#import <WTCalendarManage.h>
 
 @interface JYTViewController ()
 
@@ -18,6 +19,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSInteger count = [[WTCalendarManage sharedManage] getCountOfMonthByDate:[NSDate date]];
+    NSLog(@"当月有几天 : %ld", (long)count);
 }
 
 - (void)didReceiveMemoryWarning
